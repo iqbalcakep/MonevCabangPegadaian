679057e74b640a195f5a7b2ecd507490fb1e3ead
aurakanzaa <aurakanzaa@yahoo.co.id>
2019-02-20 15:36:24 +0700

    samaakan

--- application/controllers/LoginPage.php
+++ application/controllers/LoginPage.php
@@ -5,7 +5,7 @@ class LoginPage extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('loginPage');	
+		$this->load->view('LoginPage');	
 	}
 
 }
--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -13,7 +13,7 @@
     <title>Dashboard 3</title>
 
     <!-- Fontfaces CSS-->
-    <link href="<?php echo base_url('./asset/css/font-face.css'); ?>" rel="stylesheet" media="all">
+    <link href="<?php echo base_url(''); ?>/asset/css/font-face.css" rel="stylesheet" media="all">
     <link href="<?php echo base_url(''); ?>/asset/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <link href="<?php echo base_url(''); ?>/asset/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
     <link href="<?php echo base_url(''); ?>/asset/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
